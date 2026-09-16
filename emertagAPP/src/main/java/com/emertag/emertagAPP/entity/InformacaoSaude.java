dcf93175d52d02ab71f4c04f047300d6740d3c56
Maxwell <maxwellcadete9@gmail.com>
2026-09-16 16:10:20 -0300

    feat(entity): Criado a entidade InforamacaoSaude

---
+++ emertagAPP/src/main/java/com/emertag/emertagAPP/entity/InformacaoSaude.java
@@ -0,0 +1,54 @@
+package com.emertag.emertagAPP.entity;
+import com.emertag.emertagAPP.enums.TipoInformacaoSaude;
+import jakarta.persistence.*;
+import jakarta.websocket.Decoder.Text;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import org.springframework.cglib.core.Local;
+
+@Entity
+@Table(name = "informacao_saude")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class InformacaoSaude {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_informacao")
+    private Long idInformacao;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_perfil", nullable = false)
+    private PerfilEmergencia perfil;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 30)
+    private TipoInformacaoSaude tipo;
+
+    @Column(nullable = false, length = 255)
+    private String descricao; 
+    
+    @Column(name = "criado_em", nullable = false, updatable = false)
+    private LocalDateTime criadoEM;
+
+    @Column(name = "atualiazado_em")
+    private LocalDateTime atualizadoEM; 
+
+
+    @PrePersist
+    private void prePersist(){
+        this.criadoEM = LocalDateTime.now();
+        this.atualizadoEM = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    private void preUpdate(){
+        this.atualizadoEM = LocalDateTime.now(); 
+    }
+}
